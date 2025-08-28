75611c22d80fc6b13b95a300a12d348204173ac7
hector2725 <reshaded13@gmail.com>
2025-08-28 11:46:57 +0800

    firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbSqd-aOKgtpjdRav4-qIR2CCq1Ts0oYI",
+  authDomain: "apc-goal-tracker.firebaseapp.com",
+  projectId: "apc-goal-tracker",
+  storageBucket: "apc-goal-tracker.firebasestorage.app",
+  messagingSenderId: "90473710916",
+  appId: "1:90473710916:web:d48d918c0bd8516e670311"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
